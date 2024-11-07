275721ab06f80f039b8fbb9b06ae661e7f512fe9
Dayvid428 <dayvid180503@gmail.com>
2024-11-07 14:21:08 -0500

    añadi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MenuBar from './components/MenuBar';
 import './App.css';
+import Inicio from './pages/Inicio';
+import Nosotros from './pages/Nosotros';
+import Contacto from './pages/Contacto';
+import Productos from './pages/Productos';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/*Navbar en la parte superior */}
+        <MenuBar />
+
+        {/* Las rutas y el contenido de la página */}
+        <Routes>
+          <Route path="/Inicio" element={<Inicio />} />
+          <Route path="/Nosotros" element={<Nosotros />} />
+          <Route path="/productos" element={<Productos />} />
+          <Route path="/Contacto" element={<Contacto />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
